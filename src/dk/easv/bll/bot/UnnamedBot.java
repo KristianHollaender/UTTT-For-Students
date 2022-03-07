68e86962717838d5fd6339b697c55aba21c78d75
KristianHollaender <kristianhansen9@hotmail.dk>
2022-03-07 10:23:37 +0100

    unnamed class

--- src/dk/easv/bll/bot/UnnamedBot.java
+++ src/dk/easv/bll/bot/UnnamedBot.java
@@ -3,15 +3,26 @@ package dk.easv.bll.bot;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
 
+import java.util.List;
+import java.util.Random;
+
 public class UnnamedBot implements IBot{
+    private static final String BOTNAME = "Unnamed Bot";
+    private Random random = new Random();
+
 
     @Override
     public IMove doMove(IGameState state) {
+        List<IMove> moves = state.getField().getAvailableMoves();
+        if (moves.size() > 0) {
+            return moves.get(random.nextInt(moves.size())); /* get random move from available moves */
+        }
+        
         return null;
     }
 
     @Override
     public String getBotName() {
-        return null;
+        return BOTNAME;
     }
 }
